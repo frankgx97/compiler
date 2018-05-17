f12a132619e6dc56e65e8ac5ddaca51598ce02bb
JonathanY <1605563934@qq.com>
2018-05-17 10:57:43 +0000

    上传文件至 ''
    symbols.h是符号集，out.txt是输出文件

---
+++ symbols.h
@@ -0,0 +1,47 @@
+#define F_DEBUG
+
+#define KEY_WORD 	1
+#define SYMBOL		2
+#define ID			3
+#define NUM			4
+#define CMT			5
+#define ERROR		6
+#define SPACE		7
+
+//key words
+#define K_WHILE		5
+#define K_IF		6
+#define K_ELSE		7
+#define K_INT		8
+#define K_RETURN	9
+#define K_VOID		10
+
+//symbol
+#define O_ADD		11
+#define O_SUB		12
+#define O_MUL		13
+#define O_DIV		14
+
+#define O_LESS		15	//	<
+#define O_L_EQUAL	16	//	<=
+#define O_GREATER	17	//	>
+#define O_G_EQUAL	18	//	>=
+#define O_EQUAL		19	//	==
+#define O_U_EQUAL	20	//	!=
+
+#define O_ASSIGN	21	//	=
+#define O_SEMI		22	//	;
+#define O_COMMA		23	//	,
+
+#define O_LSBRACKER	24
+#define O_RSBRACKER	25
+#define O_LMBRACKER	26
+#define O_RMBRACKER	27
+#define O_LLBRACKER	28
+#define O_RLBRACKER	29
+#define O_LCOMMENT	30
+#define O_RCOMMENT	31
+
+#define SPACES		98
+#define COMMENT		99
+#define U_LEAGAL	100
